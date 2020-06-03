5d4403367e8c80dc73d531c4a9171a67846061c9
Juan Castaneda <juavauj@gmail.com>
2020-06-03 16:50:25 -0500

    Nuevos componentes de app ' front

---
+++ front/src/app/components/reproductor/reproductor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reproductor',
+  templateUrl: './reproductor.component.html',
+  styleUrls: ['./reproductor.component.scss']
+})
+export class ReproductorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
