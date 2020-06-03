ce1d1c25f5730d485d770656062892159ea442fa
David Rueda <hunternewlife@hotmail.com>
2020-06-03 16:50:17 -0500

    credos dos componentes

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginAdminComponent } from './components/login-admin/login-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginAdminComponent
   ],
   imports: [
     BrowserModule
