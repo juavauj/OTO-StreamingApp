3f14e51956c89443b8544b9b202282888c093e41
David Rueda <hunternewlife@hotmail.com>
2020-06-05 19:31:37 -0500

    Just updating my files

--- front/src/app/services/usuario.service.ts
+++ front/src/app/services/usuario.service.ts
@@ -3,10 +3,29 @@ import { Injectable } from '@angular/core';
 // Importar los módulos HttpClient y HttpHeaders
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class UsuarioService {
 
-  constructor() { }
+  url= 'http://localhost:3000/api/';
+  
+  constructor(
+    private _http: HttpClient
+    ) { }
+
+    registro(usuarioNuevo){
+      let params = JSON.stringify(usuarioNuevo);
+      let options = {
+        headers: new HttpHeaders({'Content-Type' : 'application/json'})
+      };
+
+      return this._http.post(
+        this.url + `registro`,
+        params,
+        options
+      ).pipe(map(res => res));
+    }
 }
+
+
