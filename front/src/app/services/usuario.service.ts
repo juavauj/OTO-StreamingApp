c4ba98d681d25bf8eea1928ae3ab187b4cfcc51b
DanielSanchez98 <ds980220@gmail.com>
2020-06-08 14:30:54 -0500

    Animacion de formularios y contenido de la clase referente a iniciar sesion

--- front/src/app/services/usuario.service.ts
+++ front/src/app/services/usuario.service.ts
@@ -15,10 +15,14 @@ export class UsuarioService {
   // Declarar la variable url de la api
   url = 'http://localhost:3000/api/';
 
+  // Declarar la variable identidad
+  public identidad;
+
   constructor(
     private _http: HttpClient
   ) { }
 
+  // ----------------------------------------------------------
   // Declarar el método del servicio registro
   registro(usuarioNuevo) {
     let params = JSON.stringify(usuarioNuevo);
@@ -34,4 +38,38 @@ export class UsuarioService {
 
   }
 
+  //----------------------------------------------------------
+  // Declarar el método del servicio iniciarSesion
+  iniciarSesion(usuarioLogueado){
+    let params = JSON.stringify(usuarioLogueado);
+    let options = {
+      headers : new HttpHeaders( { 'Content-Type' : 'application/json' } )
+    }
+    return this._http.post(
+      this.url + 'login',
+      params,
+      options
+    ).pipe(map(res => res));
+  }
+  //----------------------------------------------------------
+  // Declarar el método del servicio obtenerNombreUsuario
+  obtenerNombreUsuario(){
+    /* 
+      En una variable llamada identidad recogeremos los datos de nuestros usuarios una vez que haya iniciado sesion.
+      Estos datos se encuentran en el local storage.
+    */
+   let usuarioAutorizado = JSON.parse(localStorage.getItem('sesion'));
+
+   // Validar si localStorage esta vacio
+   if(usuarioAutorizado != 'undefined'){
+     this.identidad = usuarioAutorizado;
+
+   }else{
+     this.identidad = null;
+   }
+   
+   return this.identidad;
+
+  }
+  //----------------------------------------------------------
 }
