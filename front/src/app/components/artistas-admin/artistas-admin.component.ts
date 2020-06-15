77a9069d941e2e7dfd8a0315346c2fb090444f8d
roguerre <iv.gubri@yahoo.com>
2020-06-15 13:47:30 -0500

    consumir servicio para obtener todos los artistas

--- front/src/app/components/artistas-admin/artistas-admin.component.ts
+++ front/src/app/components/artistas-admin/artistas-admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importar el servicio de Artista
+import { ArtistaService } from '../../services/artista.service';
+
 @Component({
   selector: 'app-artistas-admin',
   templateUrl: './artistas-admin.component.html',
@@ -7,13 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistasAdminComponent implements OnInit {
 
-  filterPost = '';
+  filterArtista = '';
 
-  posts;
+  artistas;
 
-  constructor() { }
+  constructor(private artistaService: ArtistaService) { }
 
   ngOnInit(): void {
+    // Consumir servicio para obtencion de todos los artistas
+    this.artistaService.getArtistas().subscribe(
+      (response: any) => {
+        this.artistas = response.artista;
+        if (!this.artistas) {
+          console.log('No hay artistas en bdd');
+        }
+      },
+      (error) => {
+        let errorMensaje = <any>error;
+
+        if (errorMensaje != null) {
+          console.log(error);
+        }
+      }
+    );
   }
 
   delete(post){
