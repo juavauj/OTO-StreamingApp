599ec9279ba83854c56788dac3287634d2a39ca4
roguerre <iv.gubri@yahoo.com>
2020-06-16 09:00:50 -0500

    crear modelo de artista en frontend

---
+++ front/src/app/modelo/artista.ts
@@ -0,0 +1,9 @@
+export class Artista {
+  constructor(
+    public _id: String,
+    public nombre: String,
+    public estado: String,
+    public imagen: String,
+    public biografia: String
+  ) {}
+}
