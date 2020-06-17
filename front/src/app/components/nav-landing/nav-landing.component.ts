70905fe0142bf63a25217beeea954a3d3e955186
Juan Castaneda <juavauj@gmail.com>
2020-06-17 08:25:55 -0500

    Animaciones Navbar

--- front/src/app/components/nav-landing/nav-landing.component.ts
+++ front/src/app/components/nav-landing/nav-landing.component.ts
@@ -3,6 +3,11 @@ import { NavSwitchService } from '../../services/nav-switch.service';
 // Importar el servicio
 import { UsuarioService } from '../../services/usuario.service';
 
+// Importar Animaciones GSAP
+import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, Draggable, MotionPathPlugin);
+
 // Importar el manejador de rutas
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
@@ -15,35 +20,65 @@ export class NavLandingComponent implements OnInit {
   public isLoggedIn;
   public rol;
 
-  constructor(private usuarioService : UsuarioService, private _router : Router,private navSwitchService : NavSwitchService) { }
+  constructor(private usuarioService: UsuarioService, private _router: Router, private navSwitchService: NavSwitchService) { }
 
   ngOnInit(): void {
 
-    let log=JSON.parse(localStorage.getItem('logged'));
-    this.isLoggedIn=log.logged;
-    console.log('log nav',log.logged);
+    let log = JSON.parse(localStorage.getItem('logged'));
+    this.isLoggedIn = log.logged;
+    console.log('log nav', log.logged);
     this.checkRol();
   }
 
-  switchNav(status:String){
+  switchNav(status: String) {
     this.navSwitchService.switchNav(status);
   }
 
-  logOut(){
+  logOut() {
     this.usuarioService.cerrarSesion();
     this._router.navigate(['']);
 
   }
 
-  checkRol(){
-    if(localStorage.getItem('sesion')!=null){
+  checkRol() {
+    if (localStorage.getItem('sesion') != null) {
       this.rol = this.usuarioService.getRol();
-      console.log('rol nav',this.rol);
+      console.log('rol nav', this.rol);
 
     }
-    
+
 
   }
+
+  burgerClic() {
+    const header = document.querySelector('header');
+    const burgMenu = document.querySelector('.burger-menu')
+    header.classList.toggle('active');
+    burgMenu.classList.toggle('active');
+
+
+  }
+
+  /*
+    --------------------------------------------------- GSAP Animations ---------------------------------------------------------------
+    -----------------------------------------------------------------------------------------------------------------------------------  
+  */
+
+  gsapAnimatios() {
+
+    // Mostrar el navBar luego de las animaciones iniciales
+
+    (function hideNavBar() {
+      gsap.from("header", {
+        opacity: 0,
+        y: -80,
+        duration: 2,
+        delay: 6,
+        ease: "sine.out"
+      })
+    })();
+
+  };
 
 
 }
