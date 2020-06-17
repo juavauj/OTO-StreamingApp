254b61b41d68189f24695645bbabe3fed211b314
David Rueda <hunternewlife@hotmail.com>
2020-06-17 08:20:50 -0500

    nada completado nada que comentar

--- front/src/app/components/usuarios-admin/usuarios-admin.component.ts
+++ front/src/app/components/usuarios-admin/usuarios-admin.component.ts
@@ -44,8 +44,31 @@ export class UsuariosAdminComponent implements OnInit {
     })
   }
 
-  mute(post){
-    console.log('hola mundo');
+  mutearUsuario(usuario){
+    if (usuario.estado === 'activo') {
+      usuario.estado = 'inactivo';
+    } else if (usuario.estado === 'inactivo') {
+      usuario.estado = 'activo'
+    }
+
+    this.usuarioService.editarusuario(usuario._id, usuario).subscribe(
+      (response : any)=>{
+        let actualizado = response.usuario;
+        if(!actualizado){
+         alert(`no hemos podido acializar el usuario (${usuario.nombre})`);
+      } else {
+        this.usuarioService.obtenerTareas().subscribe((res:any)=>{
+          this.posts = res;
+          alert(`(${actualizado.nombre}) actualizado exitosamente`);
+        }
+      }
+      (error) =>{
+        var errorMensaje = <any>error;
+        if(errorMensaje != null){
+          console.log(error);
+        }
+      }
+    );
   }
 
 
